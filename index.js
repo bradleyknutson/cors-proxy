580771bae33e8ca8a6c9d4b7831fb497932c8ad6
Bradley Knutson <bradley@bknutson.com>
2022-01-30 19:09:59 -0700

    Add error handling

--- index.js
+++ index.js
@@ -3,18 +3,34 @@ const express = require("express");
 const app = express();
 
 app.get("/:url", async (req, res) => {
-  const response = await axios.get(req.params.url);
-  res.json(response);
+  try {
+    const response = await axios.get(req.params.url);
+    res.json(response);
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 app.post("/:url", async (req, res) => {
-  const response = await axios.post(req.params.url);
-  res.json(response);
+  try {
+    const response = await axios.post(req.params.url);
+    res.json(response);
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 app.put("/:url", async (req, res) => {
-  const response = await axios.put(req.params.url);
-  res.json(response);
+  try {
+    const response = await axios.put(req.params.url);
+    res.json(response);
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 app.delete("/:url", async (req, res) => {
-  const response = await axios.delete(req.params.url);
-  res.json(response);
+  try {
+    const response = await axios.delete(req.params.url);
+    res.json(response);
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
