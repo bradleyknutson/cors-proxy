06bdd3966e6a78740dbda5cd67826acaa05d187c
Bradley Knutson <bradley@bknutson.com>
2022-01-30 19:27:32 -0700

    Add cors header

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
